b43ebb2464f7d8c68d585bc08f0345d5d222e34d
Peter McDonald <me@petermcdonald.co.uk>
2025-09-09 14:21:19 +0100

    Implemented setup.sh
    Implemented setup.sh to replace all of the placeholders with real values
    Updated pyproject.toml to have the current lowest supported Python version
    Updated README.md with usage instructions

--- README.md
+++ README.md
@@ -3,14 +3,17 @@
 Welcome to your brand-new Python repository. This repository is intended to be used as a starting point for your Python
 projects.
 
-Things you should do before going any further:
+The repository contains a setup.sh file. Running this will carry out the following tasks:
 
-* Update the LICENSE file with the year and your name.
+* Set up the licence file with your name and the current year.
+* Set up or remove the funding links in GitHub.
+* Update bug and feature request issue templates with the specified handlers username.
+* Set up SonarQube with your server details or remove the configuration files if you are not using SonarQube.
+* Delete the setup.sh file.
+
+Things you should do:
+
 * Update pyproject.toml with your project details.
-* Update .github/FUNDING.yml with the correct GitHub username.
-* Update the issue templates in `.github/ISSUE_TEMPLATE` to suit your project including updating the assignee username.
-* Update soname configuration in `sonar-project.properties` file.
-* Update .sonarlint/connectedMode.json with your SonarQube or SonarCloud server details.
 * Update the `README.md` file to describe your project.
 
 This template assumes that you will be using uv for project management and GitHub workflows.
--- setup.sh
+++ setup.sh
@@ -1,3 +1,114 @@
+function finish {
+    # Output a finishing message and delete this script
+    printf '\r\n'
+    printf '\r\n'
+    read -rp 'I am going to delete myself now, press enter to continue'
+    rm 'setup.sh'
+}
 
-read -p "I am going to delete myself now, press enter to continue"
-# rm 'setup.sh'
+function header {
+    # Output a header for the given section
+    printf '\r\n'
+    echo "-------------------- $1 --------------------"
+    printf '\r\n'
+}
+
+header 'General Information'
+
+current_year=$(date +'%Y')
+read -rp 'What is your GitHub username? ' github_username
+
+function funding_setup {
+    # Setup funding information
+    header 'Funding Setup'
+    read -rp 'Would you like to setup funding for this project? (y/n) ' funding_y_n
+    if [ "$funding_y_n" = 'y' ]; then
+        github_funding_username=$github_username
+        read -rp 'Should funding be setup for your GitHub account? (y/n) ' github_funding_y_n
+        if [ "$github_funding_y_n" = 'n' ]; then
+            read -rp 'Enter the GitHub username to use for funding: ' github_funding_username
+        fi
+        sed -i '' -e "s/{ USERNAME }/$github_funding_username/g" '.github/FUNDING.yml'
+    else
+        printf 'Skipping funding setup and deleting .github/FUNDING.yml.'
+        rm '.github/FUNDING.yml'
+    fi
+}
+
+function setup_issue_templates {
+    # Setup issue templates
+    header 'Bug Report Templates'
+    read -rp 'Should bug reports be assigned to a user? (y/n) ' assign_bugs_y_n
+    bug_handler=$github_username
+    if [ "$assign_bugs_y_n" = 'y' ]; then
+        read -rp 'Should bug reports be assigned to you? (y/n) ' assign_bugs_you_y_n
+        if [ "$assign_bugs_you_y_n" = 'n' ]; then
+            read -rp 'Who should bugs be assigned too? ' bug_handler
+        fi
+        printf 'Assigning bugs to %s' "$bug_handler"
+        sed -i '' -e "s/{ USERNAME }/$bug_handler/g" '.github/ISSUE_TEMPLATE/bug_report.md'
+    else
+        printf 'Removing the assignment line from bug report template.'
+        sed -i '' -e 's/assignees: { USERNAME }//g' '.github/ISSUE_TEMPLATE/bug_report.md'
+    fi
+
+    header 'Issue Report Templates'
+
+    read -rp 'Should feature requests be assigned to a user? (y/n) ' assign_features_y_n
+    feature_handler=$github_username
+    if [ "$assign_features_y_n" = 'y' ]; then
+        read -rp 'Should feature requests be assigned to you? (y/n) ' assign_features_you_y_n
+        if [ "$assign_features_you_y_n" = 'n' ]; then
+            read -rp 'Who should feature requests be assigned too? ' feature_handler
+        fi
+        printf 'Assigning feature requests to %s' "$feature_handler"
+        sed -i '' -e "s/{ USERNAME }/$feature_handler/g" '.github/ISSUE_TEMPLATE/feature_request.md'
+    else
+        printf 'Removing the assignment line from bug report template.'
+        sed -i '' -e 's/assignees: { USERNAME }//g' '.github/ISSUE_TEMPLATE/feature_request.md'
+    fi
+}
+
+function setup_licence {
+    # Setup licence information
+    header "LICENSE File."
+    read -rp 'What name should the copyright be under? ' copyright_name
+    sed -i '' -e "s/{ YEAR }/$current_year/g" 'LICENSE'
+    sed -i '' -e "s/{ YOUR NAME OR YOUR COMPANY NAME }/$copyright_name/g" 'LICENSE'
+    printf 'Updated LICENSE file with your name and current year.'
+}
+
+function setup_sonar {
+    # Setup SonarQube information
+    header 'SonarQube Setup'
+    read -rp 'Would you like to setup SonarQube for this project? (y/n) ' sonar_y_n
+    if [ "$sonar_y_n" = 'y' ]; then
+        read -rp 'What is the SonarQube server URL? ' sonar_server_url
+        read -rp 'What is the SonarQube project key? ' sonar_project_key
+
+        sonar_server_url_safe="${sonar_server_url//\//\/}"
+        sonar_server_url_safe="${sonar_server_url_safe//./\.}"
+
+        printf 'Updating .sonarlint/connectedMode.json with the url and key provided.'
+        sed -i '' -e "s/{ SONARQUBE URL }/${sonar_server_url_safe}/g" '.sonarlint/connectedMode.json'
+        sed -i '' -e "s/{ SONARQUBE PROJECT KEY }/$sonar_project_key/g" '.sonarlint/connectedMode.json'
+
+        printf '\r\n'
+
+        printf 'Updating sonar-project.properties with the key and source directory provided.'
+        sed -i '' -e "s/{ SONARQUBE PROJECT KEY }/$sonar_project_key/g" 'sonar-project.properties'
+        sed -i '' -e "s/{ PROJECT SOURCE }/src/g" 'sonar-project.properties'
+    else
+        printf 'Removing Sonar related files.'
+        rm 'sonar-project.properties'
+        rm -rf '.sonarlint'
+        rm '.github/workflows/sonarqube-analysis.yml'
+    fi
+}
+
+setup_licence
+funding_setup
+setup_issue_templates
+setup_sonar
+
+finish
